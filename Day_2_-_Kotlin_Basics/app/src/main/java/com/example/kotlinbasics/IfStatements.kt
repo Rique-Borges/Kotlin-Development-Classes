6d2302a119f54fce2deb6c20e2b81156c0e516a7
Rique-Borges <henriqueribeiroborges@gmail.com>
2024-07-15 18:48:32 -0300

    Estudo de If-Else e palavra chave In

--- Day_2_-_Kotlin_Basics/app/src/main/java/com/example/kotlinbasics/IfStatements.kt
+++ Day_2_-_Kotlin_Basics/app/src/main/java/com/example/kotlinbasics/IfStatements.kt
@@ -1,2 +1,23 @@
 package com.example.kotlinbasics
 
+fun main(){
+    var age = 0
+    println("insira sua idade")
+    val enteredValue = readln()//Ler o que o usuário inseriu
+    age = enteredValue.toInt() //converter para integer
+
+    // Operadores de Comparação
+    // (>)  Maior Que
+    // (<)  Menor Que
+    // (==) Igual
+    // (>=) Maior ou Igual
+    // (<=) Menor ou Igual
+
+    if (age in 18..39){
+        println("you can come in")
+    }else if (age < 40) {
+        println("you cannot come in, you're too old")
+    }else{
+        println("you cannot come in, you're too old")
+    }
+}
